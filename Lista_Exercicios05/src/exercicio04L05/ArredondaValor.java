ef2bbce0353fdc740a87b28f9b1ad0c05c0a1816
Brenno-Silva01 <brenno877@gmail.com>
2025-06-19 19:54:32 -0300

    Adicionando exercicio 04 Lista 05

---
+++ Lista_Exercicios05/src/exercicio04L05/ArredondaValor.java
@@ -0,0 +1,15 @@
+package exercicio04L05;
+
+public class ArredondaValor {
+    public static void main(String[] args) {
+
+        // Arredondando 5.2
+        System.out.println("O arredondamento de 5.2 é: " + Math.round(5.2));
+
+        // Arredondando 5.6
+        System.out.println("O arredondamento de 5.6 é: " + Math.round(5.6));
+
+        // Arredondando -5.8
+        System.out.println("O arredondamento de -5.8 é: " + Math.round(-5.8));
+    }
+}
